4848fa55fd52130f1a3e2419a4c03149a7906e5e
Shivam Sharma <shivam.sharma@myparkplus.com>
2023-01-03 17:47:42 +0530

    scss added

--- rollup.config.js
+++ rollup.config.js
@@ -32,6 +32,6 @@ export default [
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: [/\.css$/],
+    external: [/\.scss$/],
   },
 ];
